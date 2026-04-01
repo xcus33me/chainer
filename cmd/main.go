cf3d4ab15b26d69ff1b50e6c59c929ced5053e12
xcus33me <HarvieOrigin@yandex.ru>
2026-04-01 11:21:39 +0300

    testing

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-	"log/slog"
-	"net"
-	"os"
+	"context"
+	"log"
 	"time"
 
 	"github.com/xcus33me/chainer/node"
-	"github.com/xcus33me/chainer/proto"
-	"github.com/xcus33me/chainer/utils"
-	"google.golang.org/grpc"
 )
 
 func main() {
-	node := node.NewNode()
+	makeNode(":3000", []string{})
+	time.Sleep(1 * time.Second)
+	makeNode(":4000", []string{":3000"})
 
-	opts := []grpc.ServerOption{}
-	grpcServer := grpc.NewServer(opts...)
+	// go func() {
+	// 	for {
+	// 		time.Sleep(2 * time.Second)
+	// 		utils.CreateTransaction()
+	// 	}
+	// }()
+	select {}
+}
 
-	ln, err := net.Listen("tcp", ":3000")
-	if err != nil {
-		slog.Error("Failed to listen on :3000", "err", err)
-		os.Exit(1)
-	}
-	proto.RegisterNodeServer(grpcServer, node)
+func makeNode(listenAddr string, bootstrapNodes []string) *node.Node {
+	n := node.NewNode()
+	go n.Start(listenAddr)
 
-	slog.Info("node running on port :3000")
-
-	go func() {
-		for {
-			time.Sleep(time.Second)
-			utils.CreateTransaction()
+	if len(bootstrapNodes) >= 1 {
+		if err := n.BootstrapNetwork(context.Background(), bootstrapNodes); err != nil {
+			log.Fatal(err)
 		}
-	}()
-	grpcServer.Serve(ln)
+	}
+
+	return n
 }
--- go.mod
+++ go.mod
@@ -10,9 +10,14 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/zerolog v1.35.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
 	golang.org/x/net v0.48.0 // indirect
-	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/sys v0.42.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
