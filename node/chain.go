b2cd8e084c975f8b19c4a7c2be70cc0868e4948e
xcus33me <HarvieOrigin@yandex.ru>
2026-04-05 17:26:51 +0300

    feat: genesis block prepare

--- node/chain.go
+++ node/chain.go
@@ -1,10 +1,12 @@
 package node
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 
 	"github.com/xcus33me/chainer/core"
+	"github.com/xcus33me/chainer/crypto"
 	"github.com/xcus33me/chainer/proto"
 )
 
@@ -44,10 +46,13 @@ type Chain struct {
 }
 
 func NewChain(bs BlockStorer) *Chain {
-	return &Chain{
+	chain := &Chain{
 		blockStore: bs,
 		headers:    NewHeaderList(),
 	}
+	chain.addBlock(createGenesisBlock())
+
+	return chain
 }
 
 func (c *Chain) Height() int {
@@ -55,6 +60,14 @@ func (c *Chain) Height() int {
 }
 
 func (c *Chain) AddBlock(b *proto.Block) error {
+	if err := c.ValidateBlock(b); err != nil {
+		return err
+	}
+
+	return c.addBlock(b)
+}
+
+func (c *Chain) addBlock(b *proto.Block) error {
 	// Add the header to the list of headers
 	c.headers.Add(b.Header)
 	// validation
@@ -75,4 +88,36 @@ func (c *Chain) GetBlockByHeight(height int) (*proto.Block, error) {
 func (c *Chain) GetBlockByHash(hash []byte) (*proto.Block, error) {
 	hashHex := hex.EncodeToString(hash)
 	return c.blockStore.Get(hashHex)
+}
+
+func (c *Chain) ValidateBlock(b *proto.Block) error {
+	// Validate the signature of block
+	if !core.VerifyBlock(b) {
+		return fmt.Errorf("invalid block signature")
+	}
+
+	// Validate if the prevHash is the actual hash of the curr block
+	currBlock, err := c.GetBlockByHeight(c.Height())
+	if err != nil {
+		return err
+	}
+
+	hash := core.HashBlock(currBlock)
+	if !bytes.Equal(hash, b.Header.PrevHash) {
+		return fmt.Errorf("invalid prev block hash")
+	}
+
+	return nil
+}
+
+func createGenesisBlock() *proto.Block {
+	privKey := crypto.GeneratePrivateKey()
+	block := &proto.Block{
+		Header: &proto.Header{
+			Version: 1,
+		},
+	}
+	core.SignBlock(privKey, block)
+
+	return block
 }
--- node/chain_test.go
+++ node/chain_test.go
@@ -4,35 +4,58 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/xcus33me/chainer/core"
+	"github.com/xcus33me/chainer/crypto"
+	"github.com/xcus33me/chainer/proto"
 	"github.com/xcus33me/chainer/utils"
 )
+
+func randomBlock(t *testing.T, chain *Chain) *proto.Block {
+	privKey := crypto.GeneratePrivateKey()
+
+	block := utils.RandomBlock()
+	prevBlock, err := chain.GetBlockByHeight(chain.Height())
+	require.Nil(t, err)
+	block.Header.PrevHash = core.HashBlock(prevBlock)
+	core.SignBlock(privKey, block)
+
+	return block
+}
+
+func TestNewChain(t *testing.T) {
+	chain := NewChain(NewMemoryBlockStore())
+	assert.Equal(t, 0, chain.Height())
+	_, err := chain.GetBlockByHeight(0)
+	assert.Nil(t, err)
+}
 
 func TestAddBlock(t *testing.T) {
 	chain := NewChain(NewMemoryBlockStore())
 
 	for i := 0; i < 100; i++ {
-		var (
-			block     = utils.RandomBlock()
-			blockHash = core.HashBlock(block)
-		)
-		assert.Nil(t, chain.AddBlock(block))
+		block := randomBlock(t, chain)
+		blockHash := core.HashBlock(block)
+
+		require.Nil(t, chain.AddBlock(block))
 
 		fetchedBlock, err := chain.GetBlockByHash(blockHash)
-		assert.Nil(t, err)
-		assert.Equal(t, block, fetchedBlock)
+		require.Nil(t, err)
+		require.Equal(t, block, fetchedBlock)
 
-		fetchedBlockByHeight, err := chain.GetBlockByHeight(i)
-		assert.Nil(t, err)
-		assert.Equal(t, block, fetchedBlockByHeight)
+		fetchedBlockByHeight, err := chain.GetBlockByHeight(i + 1)
+		require.Nil(t, err)
+		require.Equal(t, block, fetchedBlockByHeight)
 	}
 }
 
 func TestChainHeight(t *testing.T) {
 	chain := NewChain(NewMemoryBlockStore())
+
 	for i := 0; i < 100; i++ {
-		b := utils.RandomBlock()
-		assert.Nil(t, chain.AddBlock(b))
-		assert.Equal(t, chain.Height(), i)
+		b := randomBlock(t, chain)
+
+		require.Nil(t, chain.AddBlock(b))
+		require.Equal(t, chain.Height(), i+1)
 	}
 }
